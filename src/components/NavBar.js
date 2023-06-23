55525ef08fcb1636cb1a4656a7acf85b4ad29ed6
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2023-06-23 16:51:44 -0300

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+function NavBar() {
+  return (
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href='/'>B9 GAMING</a>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
