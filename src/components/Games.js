324c98cc4744b9f3ff2f4001978ea93b23dc1e7e
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2023-06-24 02:38:33 -0300

    Games Filter

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,4 +1,4 @@
-import { Card, Stack, CardBody, CardFooter, Image, Heading, Text, Divider, Button, ButtonGroup } from '@chakra-ui/react'
+import { Card, Stack, CardBody, CardFooter, Image, Heading, Text, Divider, Button, ButtonGroup, Tabs, TabList, Tab, TabIndicator, PinInput, PinInputField } from '@chakra-ui/react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './games.css'
@@ -10,9 +10,63 @@ function Games() {
             <div className="container">
                 <div className="row">
                     <div className="col-12">
-                        <h3 className="main-title">Nossos jogos
-                            <hr />
+                        <h3 id="main-title">Categorias
                         </h3>
+                        <div id='game-tabs'>
+                            <Tabs position="relative" variant="unstyled" defaultIndex={0}>
+                                <TabList justifyContent="center" colorScheme="green">
+                                    <Tab color="green.700"
+                                        _active={{ color: "green.50" }}
+                                        _focus={{ color: "green.50" }}
+                                    >All Games</Tab>
+                                    <Tab color="green.700"
+                                        _active={{ color: "green.50" }}
+                                        _focus={{ color: "green.50" }} >Shooter</Tab>
+                                    <Tab color="green.700"
+                                        _active={{ color: "green.50" }}
+                                        _focus={{ color: "green.50" }} >RPG</Tab>
+                                    <Tab color="green.700"
+                                        _active={{ color: "green.50" }}
+                                        _focus={{ color: "green.50" }}>Fighting</Tab>
+                                    <Tab color="green.700"
+                                        _active={{ color: "green.50" }}
+                                        _focus={{ color: "green.50" }}>Battle Royale</Tab>
+                                    <Tab color="green.700"
+                                        _active={{ color: "green.50" }}
+                                        _focus={{ color: "green.50" }} >MOBA</Tab>
+                                    <Tab color="green.700"
+                                        _active={{ color: "green.50" }}
+                                        _focus={{ color: "green.50" }} >Strategy</Tab>
+                                    <Tab color="green.700"
+                                        _active={{ color: "green.50" }}
+                                        _focus={{ color: "green.50" }} >MMO</Tab>
+                                    <Tab color="green.700"
+                                        _active={{ color: "green.50" }}
+                                        _focus={{ color: "green.50" }}>Card Game</Tab>
+                                    <Tab color="green.700"
+                                        _active={{ color: "green.50" }}
+                                        _focus={{ color: "green.50" }} >Racing</Tab>
+                                    <Tab color="green.700"
+                                        _active={{ color: "green.50" }}
+                                        _focus={{ color: "green.50" }}>Fantasy</Tab>
+                                    <Tab color="green.700"
+                                        _active={{ color: "green.50" }}
+                                        _focus={{ color: "green.50" }}>Social</Tab>
+                                    <Tab color="green.700"
+                                        _active={{ color: "green.50" }}
+                                        _focus={{ color: "green.50" }} >Sports</Tab>
+
+                                </TabList>
+                                <TabIndicator
+                                    mt="-1.5px"
+                                    height="2px"
+                                    bg="green.500"
+                                    borderRadius="1px"
+                                />
+                            </Tabs>
+                        </div>
+
+                        <hr />
                     </div>
 
                     <div className="col-md-4 card-game">
@@ -112,7 +166,7 @@ function Games() {
                         </Card>
 
                     </div>
-                    
+
                     <div className="col-md-4 card-game">
                         <Card maxW='sm' bg="gray.800" color={"white"}>
                             <CardBody>
@@ -214,6 +268,15 @@ function Games() {
 
 
                 </div>
+
+                <div id='input-fild'>
+                    <PinInput color={"white"} defaultValue='123'>
+                        <PinInputField  />
+                        <PinInputField marginLeft={'7px'} />
+                        <PinInputField marginLeft={'7px'} />
+                    </PinInput>
+                </div>
+
 
 
             </div>
