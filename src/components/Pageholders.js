663a866907a0a8459601e94a1a7a4048e0a0087a
Heitor Brunini <heitorbrunini.ifpb@gmail.com>
2023-07-19 23:57:31 -0300

    Update Pageholders.js

--- src/components/Pageholders.js
+++ src/components/Pageholders.js
@@ -1,13 +1,24 @@
 import Pagination from 'react-bootstrap/Pagination';
+import React, { useState } from 'react';
 
-function Pageholders({ setFirstGame}) {
+function Pageholders({ setFirstGame }) {
 
-  let active = 1;
+  let [active, SetActive] = useState(1);
   let items = [];
   
+  const handlePaginationItemClick = (number) => {
+    SetActive(number);
+    if (number !== 1) {
+      setFirstGame(9 * (number - 1));
+    } else {
+      setFirstGame(0);
+    }
+    window.scrollBy(0, -1600); // Scroll de 400px para cima
+  };
+
   for (let number = 1; number <= 5; number++) {
     items.push(
-      <Pagination.Item key={number} onClick={()=>{ number!==1? setFirstGame(9 * (number-1)) :setFirstGame(0); }} active={number === active} >
+      <Pagination.Item key={number} onClick={() => handlePaginationItemClick(number)} active={number === active} >
         {number}
       </Pagination.Item>,
     );
@@ -18,7 +29,6 @@ function Pageholders({ setFirstGame}) {
       <Pagination size="lg">{items}</Pagination>
     </div>
   )
-
 }
 
 export default Pageholders;
