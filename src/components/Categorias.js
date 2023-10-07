229aea9e2ddd0e580050a26a4ea1c76aaf908e9e
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2023-10-07 19:30:57 -0300

    Update Categorias.js

--- src/components/Categorias.js
+++ src/components/Categorias.js
@@ -1,18 +1,41 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 function Categorias({responseData, setResponseData}) {
 
+  function whatDoClick(Categoria){
+    console.log(Categoria);
+  }
+  
+  var categorias = ['All games'];
+  
+  // Verifique se responseData é definido antes de acessar a propriedade 'array'
+  if (responseData ) {
+    
+    responseData.forEach(element => {
+      if (!categorias.includes(element.genre)) {
+        
+        categorias.push(element.genre);
+      }
+  
+    });
+    
+  }
+
+  let Buttons = [];
+
+  categorias.forEach( (categoria) => {
+      Buttons.push(
+        <button class="btn btn-outline-success me-2" onClick={ () => whatDoClick( categoria )} type="button" style={{ width: '150px' }}>{categoria}</button>
+      )
+    }
+  )
+
+
   return (
     <nav class="navbar"  data-bs-theme="dark">
-      <form class="container-fluid justify-content-start">
-        <button class="btn btn-outline-success me-2" type="button">Main button</button>
-        <button class="btn btn-outline-success me-2" type="button">Main button</button>
-        <button class="btn btn-outline-success me-2" type="button">Main button</button>
-        <button class="btn btn-outline-success me-2" type="button">Main button</button>
-        <button class="btn btn-outline-success me-2" type="button">Main button</button>
-
+      <form   class="container-fluid justify-content-start flex-nowrap">
+        {Buttons}        
       </form>
     </nav>
   );
